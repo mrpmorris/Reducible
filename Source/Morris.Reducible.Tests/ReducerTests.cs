949290de22456f63c50ffe6238a59d63565bc634
Tim <t.jones@timmoth.com>
2022-04-10 10:26:34 +0100

    Added Conditional Reducer tests

--- Source/Morris.Reducible.Tests/ReducerTests.cs
+++ Source/Morris.Reducible.Tests/ReducerTests.cs
@@ -4,18 +4,18 @@ namespace Morris.Reducible.Tests
 {
 	public class ReducerTests
     {
-	    public record CounterState(int Count);
-	    public record CountAction(int Delta);
+	    record CounterState(int Counter);
+	    record IncrementCounter(int Amount);
 
 		[Fact]
 	    public void SimpleReducerTest()
 	    {
 			//Given
 			var initialState = new CounterState(0);
-			var action = new CountAction(1);
+			var action = new IncrementCounter(1);
 			var counterIncrementCounterReducer = Reducer
-			 .Given<CounterState, CountAction>()
-			 .Then((state, delta) => (true, state with { Count = state.Count + delta.Delta}));
+			 .Given<CounterState, IncrementCounter>()
+			 .Then((state, delta) => (true, state with { Counter = state.Counter + delta.Amount}));
 
 			//When
 			(bool changed, var counterState) = counterIncrementCounterReducer.Invoke(initialState, action);
@@ -23,6 +23,44 @@ namespace Morris.Reducible.Tests
 			//Then
 			Assert.True(changed);
 			Assert.Equal(new CounterState(1), counterState);
+	    }	
+	    
+	    [Fact]
+	    public void ConditionalReducerModifysStateWhenConditionPasses()
+	    {
+			//Given
+			var initialState = new CounterState(0);
+			var action = new IncrementCounter(1);
+			var counterIncrementCounterReducer = Reducer
+				.Given<CounterState, IncrementCounter>()
+				.When((state, delta) => state.Counter < 2)
+				.Then((state, delta) => state with { Counter = state.Counter + delta.Amount });
+
+			//When
+			(bool changed, var counterState) = counterIncrementCounterReducer.Invoke(initialState, action);
+
+			//Then
+			Assert.True(changed);
+			Assert.Equal(new CounterState(1), counterState);
+	    }
+
+	    [Fact]
+	    public void ConditionalReducerDoesNotModifyStateWhenConditionFails()
+	    {
+		    //Given
+		    var initialState = new CounterState(0);
+		    var action = new IncrementCounter(1);
+		    var counterIncrementCounterReducer = Reducer
+			    .Given<CounterState, IncrementCounter>()
+			    .When((state, delta) => state.Counter > 2)
+			    .Then((state, delta) => state with { Counter = state.Counter + delta.Amount });
+
+		    //When
+		    (bool changed, var counterState) = counterIncrementCounterReducer.Invoke(initialState, action);
+
+		    //Then
+		    Assert.False(changed);
+		    Assert.Equal(new CounterState(0), counterState);
 	    }
 	}
 }
