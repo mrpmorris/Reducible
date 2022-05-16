d51975d05010f95132941c66790f657cfe496348
x <x@x.com>
2022-05-16 17:41:20 +0100

    ImmutableArray OptimizedDelta

--- Source/Morris.Reducible/WhenImmutableArrayReducedByBuilder.cs
+++ Source/Morris.Reducible/WhenImmutableArrayReducedByBuilder.cs
@@ -3,37 +3,42 @@ using System.Collections.Immutable;
 
 namespace Morris.Reducible;
 
-public class WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta>
+public class WhenImmutableArrayReducedByBuilder<TState, TElement, TDeltaIn, TOptimizedDelta>
 {
-	private readonly Builder<TState, TDelta> SourceBuilder;
+	private readonly Builder<TState, TDeltaIn> SourceBuilder;
 	private readonly Func<TState, ImmutableArray<TElement>> SubStateSelector;
-	private readonly Func<TElement, TDelta, ReducerResult<TElement>> ElementReducer;
+	private readonly Func<TDeltaIn, TOptimizedDelta> OptimizeDelta;
+	private readonly Func<TElement, TOptimizedDelta, ReducerResult<TElement>> ElementReducer;
 
 	internal WhenImmutableArrayReducedByBuilder(
-		Builder<TState, TDelta> sourceBuilder,
+		Builder<TState, TDeltaIn> sourceBuilder,
 		Func<TState, ImmutableArray<TElement>> subStateSelector,
-		Func<TElement, TDelta, ReducerResult<TElement>> elementReducer)
+		Func<TDeltaIn, TOptimizedDelta> optimizeDelta,
+		Func<TElement, TOptimizedDelta, ReducerResult<TElement>> elementReducer)
 	{
 		SourceBuilder = sourceBuilder ?? throw new ArgumentNullException(nameof(sourceBuilder));
 		SubStateSelector = subStateSelector ?? throw new ArgumentNullException(nameof(subStateSelector));
+		OptimizeDelta = optimizeDelta ?? throw new ArgumentNullException(nameof(optimizeDelta));
 		ElementReducer = elementReducer ?? throw new ArgumentNullException(nameof(elementReducer));
 	}
 
-	public Func<TState, TDelta, ReducerResult<TState>> Then(Func<TState, ImmutableArray<TElement>, TState> mapper)
+	public Func<TState, TDeltaIn, ReducerResult<TState>> Then(Func<TState, ImmutableArray<TElement>, TState> mapper)
 	{
 		if (mapper is null)
 			throw new ArgumentNullException(nameof(mapper));
 
-		Func<TState, TDelta, ReducerResult<TState>> process = (state, delta) =>
+		Func<TState, TDeltaIn, ReducerResult<TState>> process = (state, delta) =>
 		{
 			ImmutableArray<TElement> elements = SubStateSelector(state);
+
+			TOptimizedDelta optimizedDelta = OptimizeDelta(delta);
 
 			var arrayBuilder = ImmutableArray.CreateBuilder<TElement>();
 
 			bool anyChanged = false;
 			for (int o = 0; o < elements.Length; o++)
 			{
-				(bool changed, TElement element) = ElementReducer(elements[o], delta);
+				(bool changed, TElement element) = ElementReducer(elements[o], optimizedDelta);
 				arrayBuilder.Add(element);
 				if (changed)
 					anyChanged = true;
--- Source/Morris.Reducible/WhenImmutableArrayReducedByBuilderExtensions.cs
+++ Source/Morris.Reducible/WhenImmutableArrayReducedByBuilderExtensions.cs
@@ -5,12 +5,28 @@ namespace Morris.Reducible;
 
 public static class WhenImmutableArrayReducedByBuilderExtensions
 {
-	public static WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta>
+	public static WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta, TDelta>
 		WhenReducedBy<TState, TElement, TDelta>(
 			this Builder<TState, TDelta> sourceBuilder,
 			Func<TState, ImmutableArray<TElement>> subStateSelector,
 			Func<TElement, TDelta, ReducerResult<TElement>> elementReducer)
 		=>
-			new WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta>(sourceBuilder, subStateSelector, elementReducer);
+			new WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta, TDelta>(
+				sourceBuilder,
+				subStateSelector,
+				delta => delta,
+				elementReducer);
 
+	public static WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta, TOptimizedDelta>
+		WhenReducedBy<TState, TElement, TDelta, TOptimizedDelta>(
+			this Builder<TState, TDelta> sourceBuilder,
+			Func<TState, ImmutableArray<TElement>> subStateSelector,
+			Func<TDelta, TOptimizedDelta> optimizeDelta,
+			Func<TElement, TOptimizedDelta, ReducerResult<TElement>> elementReducer)
+		=>
+			new WhenImmutableArrayReducedByBuilder<TState, TElement, TDelta, TOptimizedDelta>(
+				sourceBuilder,
+				subStateSelector,
+				optimizeDelta,
+				elementReducer);
 }
